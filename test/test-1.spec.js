b73669371cd513fb01a2a06652b528116662ad17
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-19 20:46:59 -0500

    Creating the first suite of tests

--- test/test-1.spec.js
+++ test/test-1.spec.js
@@ -1,7 +1,12 @@
+const Explorer = require('./../app/explorer')
 
 describe("Creating a set of tests for the Explorer module", () => {
 
     test("Test case 1", () => {
         
+        const fabian = new Explorer('Fabian', 23)
+
+        expect(fabian.name).toBe('Fabian');
+        expect(fabian.age).toBe(23)
     })
 })
